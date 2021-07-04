fd21f901e647a074d1d04c686f1b20586e36586c
sitifardamaulina <80209705+sitifardamaulina@users.noreply.github.com>
2021-07-04 14:49:37 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+
+import Hillcipher.Decrypt;
+import Hillcipher.Encrypt;
+import IO.Reader;
+import IO.Writter;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // read file
+        String readedFile = Reader.readFile("src/data/source.txt");
+        System.out.println("Source File : " + readedFile);
+        // encrypt file
+        String encryptedText = Encrypt.encrypt(readedFile);
+        System.out.println("Encrypted File : " + encryptedText);
+        // write encrypted file
+        Writter.writeFile("src/data/encrypted.txt", encryptedText);
+        // read encrypted file
+        String encryptedFile = Reader.readFile("src/data/encrypted.txt");
+        // decrupt file
+        String decryptedFile = Decrypt.decrypt(encryptedFile);
+        System.out.println("Decrypted File : " + decryptedFile);
+        // write decrypted file
+        Writter.writeFile("src/data/decrypted.txt", decryptedFile);
+    }
+}
